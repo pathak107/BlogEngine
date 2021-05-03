bd1e69548fe433c72690205b93bb333e0cd65f35
Shubham <smartshubhampathak@gmail.com>
2021-05-03 19:37:28 +0530

    decided all the models

---
+++ src/api/v1/models/category.js
@@ -0,0 +1,22 @@
+// "Category":
+//     {
+//       "slug": "getting-started",
+//       "id": "5ddc9063c35e7700383b27e0",
+//       "name": "Getting Started",
+//       "description": null,
+//       "feature_image": null,
+//       "visibility": "public",
+//       "meta_title": null,
+//       "meta_description": null,
+//       "og_image": null,
+//       "og_title": null,
+//       "og_description": null,
+//       "twitter_image": null,
+//       "twitter_title": null,
+//       "twitter_description": null,
+//       "codeinjection_head": null,
+//       "codeinjection_foot": null,
+//       "canonical_url": null,
+//       "accent_color": null,
+//       "url": "https://docs.ghost.io/tag/getting-started/"
+//     }
---
+++ src/api/v1/models/user.js
@@ -0,0 +1,7 @@
+// User database for members, intially can implement just the email feature
+// as it will already have the email of user but later
+// it will help to include premium features for selected users only who have paid
+// for some subscription
+// ex a subscription variable which will determine the validity and a
+// premiumUser variable for true or false and will mail notification to premiumUsers
+// when a new premium post is posted
